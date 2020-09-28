72265cfca0fde971fc126aa85002e40c803416b5
Shyamin Ayesh <me@shyamin.com>
2020-09-28 20:07:57 +0530

    move index to public directory

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
  * This will start serving the request by handing over
  * it to the core.
  */
-include_once './app/init.php';
+include_once '../app/init.php';
 $App = new Avanciro\Skel\Core\App;
 $App::dispatch();
 
